0c8110621ad86976773ae6f86daf1a7b47c53847
Murad <muradhossen5267@gmail.com>
2022-07-20 12:42:33 +0600

    storyUpdate added

---
+++ src/components/pages/StoryUpdate.js
@@ -0,0 +1,12 @@
+import StoryUpdateForm from '../StoryUpdateForm';
+export default function StoryUpdate(){
+   
+    console.log('story Update rendered');
+
+
+    return(
+        <div>
+            <StoryUpdateForm/>
+        </div>
+    )
+}
